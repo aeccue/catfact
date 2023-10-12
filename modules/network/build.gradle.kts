4d1ea9a357ae801f36f887ace5ce31ba05582733
Aaron Chan <develop@aeccue.com>
2023-10-12 23:57:22 +0900

    Created network module

---
+++ modules/network/build.gradle.kts
@@ -0,0 +1,61 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.serialization)
+
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.android.hilt)
+}
+
+android {
+    namespace = "jp.speakbuddy.catfact.network"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        compileOptions {
+            allWarningsAsErrors = true
+            jvmTarget = JavaVersion.VERSION_11.toString()
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.kotlin.stdlib)
+    api(libs.kotlinx.serialization.json)
+
+    implementation(libs.android.hilt)
+    kapt(libs.android.hilt.compiler)
+
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.cio)
+    implementation(libs.ktor.serialization.json)
+}
+
+kapt {
+    correctErrorTypes = true
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,3 +15,4 @@ dependencyResolutionManagement {
 rootProject.name = "Cat Facts"
 
 include(":app")
+include(":modules:network")
