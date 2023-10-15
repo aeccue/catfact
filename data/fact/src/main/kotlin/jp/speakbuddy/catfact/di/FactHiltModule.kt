0b4064da01dd492ed999d7307afccc7229e2aa60
Aaron Chan <develop@aeccue.com>
2023-10-15 12:59:09 +0900

    Updated FactRepository
    - FactRepository now uses external modules:
      - local data source has been moved to database module
      - remote data source has been moved to service module

--- data/fact/src/main/kotlin/jp/speakbuddy/catfact/di/FactHiltModule.kt
+++ data/fact/src/main/kotlin/jp/speakbuddy/catfact/di/FactHiltModule.kt
@@ -1,51 +1,16 @@
 package jp.speakbuddy.catfact.di
 
-import android.content.Context
-import androidx.datastore.core.DataStore
-import androidx.datastore.core.DataStoreFactory
-import androidx.datastore.core.Serializer
-import androidx.datastore.dataStoreFile
 import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import jp.speakbuddy.catfact.fact.data.model.FactResponse
-import jp.speakbuddy.catfact.fact.data.model.FactResponseSerializer
 import jp.speakbuddy.catfact.fact.data.repository.FactRepository
 import jp.speakbuddy.catfact.fact.data.repository.FactRepositoryImpl
-import jp.speakbuddy.catfact.fact.data.source.FactDataStore
-import jp.speakbuddy.catfact.fact.data.source.FactLocalDataSource
-import jp.speakbuddy.catfact.fact.data.source.FactRemoteDataSource
-import jp.speakbuddy.catfact.fact.data.source.FactService
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-internal interface DataStoreModule {
-
-    companion object {
-
-        @Provides
-        @Singleton
-        fun provideFactDataStore(
-            @ApplicationContext context: Context,
-            serializer: Serializer<FactResponse>
-        ): DataStore<FactResponse> =
-            DataStoreFactory.create(serializer = serializer) {
-                context.dataStoreFile("fact.pb")
-            }
-    }
-
-    @Binds
-    fun bindSerializer(serializer: FactResponseSerializer): Serializer<FactResponse>
-
-    @Binds
-    fun bindLocalDataSource(source: FactDataStore): FactLocalDataSource
-
-    @Binds
-    fun bindRemoteDataSource(source: FactService): FactRemoteDataSource
+internal interface FactHiltModule {
 
     @Binds
     fun bindRepository(repository: FactRepositoryImpl): FactRepository
--- data/fact/src/main/kotlin/jp/speakbuddy/catfact/fact/data/repository/FactRepository.kt
+++ data/fact/src/main/kotlin/jp/speakbuddy/catfact/fact/data/repository/FactRepository.kt
@@ -1,9 +1,10 @@
 package jp.speakbuddy.catfact.fact.data.repository
 
-import jp.speakbuddy.catfact.fact.data.model.Fact
-import jp.speakbuddy.catfact.fact.data.model.asFact
-import jp.speakbuddy.catfact.fact.data.source.FactLocalDataSource
-import jp.speakbuddy.catfact.fact.data.source.FactRemoteDataSource
+import jp.speakbuddy.catfact.data.model.Fact
+import jp.speakbuddy.catfact.database.dao.FactDao
+import jp.speakbuddy.catfact.database.model.FactEntity
+import jp.speakbuddy.catfact.service.CatFactService
+import jp.speakbuddy.catfact.service.model.FactApiResponse
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
@@ -16,18 +17,30 @@ interface FactRepository {
 }
 
 internal class FactRepositoryImpl @Inject constructor(
-    private val localDataSource: FactLocalDataSource,
-    private val remoteDataSource: FactRemoteDataSource
+    private val localDataSource: FactDao,
+    private val remoteDataSource: CatFactService
 ) : FactRepository {
 
     override fun fetch(): Flow<Fact> =
         localDataSource
-            .fetch()
+            .fetchLatest()
             .map { it.asFact }
 
     override suspend fun refresh(): Boolean =
         remoteDataSource.fetch()?.let { newFact ->
-            localDataSource.update(newFact)
+            localDataSource.add(newFact.asEntity)
             true
         } ?: false
 }
+
+private inline val FactEntity.asFact: Fact
+    get() = Fact(
+        fact = fact,
+        length = length
+    )
+
+private inline val FactApiResponse.asEntity: FactEntity
+    get() = FactEntity(
+        fact = fact,
+        length = length
+    )
