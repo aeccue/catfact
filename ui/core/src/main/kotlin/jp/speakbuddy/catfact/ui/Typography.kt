7321874f56ac86781f9e3f3bc4e26f724a6ea967
Aaron Chan <develop@aeccue.com>
2023-10-15 18:50:38 +0900

    Added handwritten font

--- ui/core/src/main/kotlin/jp/speakbuddy/catfact/ui/Typography.kt
+++ ui/core/src/main/kotlin/jp/speakbuddy/catfact/ui/Typography.kt
@@ -2,26 +2,30 @@ package jp.speakbuddy.catfact.ui
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import jp.speakbuddy.catfact.ui.core.R
 
-// Set of Material typography styles to start with
+val SchoolBellFontFamily = FontFamily(
+    Font(R.font.schoolbell, FontWeight.Normal)
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = SchoolBellFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = SchoolBellFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
     ),
     labelSmall = TextStyle(
         fontFamily = FontFamily.Default,
@@ -30,5 +34,4 @@ val Typography = Typography(
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */
 )
